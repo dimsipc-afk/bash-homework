edec69292bc1bd3c81c44bf762e60b07a319da2f
dimsipc-afk <dimsipc@gmail.com>
2026-03-22 19:43:42 +0300

    Create user_profile.sh

---
+++ user_profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ 
+# Переменные профиля пользователя
+name="Dima"
+age="18"
+city="Pochinki"
+ 
+# Вывод информации
+echo "=== User Profile ==="
+echo " Name: $name"
+echo " Age: $age"
+echo " City: $city"
